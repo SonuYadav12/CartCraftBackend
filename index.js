1ea7216b87d10e63ac08abd5bf59a6e6acf7d75b
Sonu Kumar Yadav <yadavsonukumar036@gmail.com>
2024-04-05 00:33:53 +0530

    cloudinary

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const fs = require("fs");
-const path = require("path"); // Import the path module
+const path = require("path"); 
 const connectDB = require("./db/connectDB");
 const cloudinary = require("cloudinary").v2; 
 dotenv.config();
@@ -23,42 +23,42 @@ cloudinary.config({
 });
 
 // Image storage engine using Multer
-const storage = multer.memoryStorage(); // Use memory storage for file buffer
+const storage = multer.memoryStorage(); 
 const upload = multer({ storage: storage });
 
 
 app.post("/upload", upload.single("product"), async (req, res) => {
   try {
-    // Check if a file was uploaded
+   
     if (!req.file) {
       return res.status(400).json({ message: 'No file uploaded' });
     }
 
-    // Save the uploaded file to the desired directory
+    
     const localFilePath = path.join(__dirname, "upload/images", req.file.originalname);
     fs.writeFileSync(localFilePath, req.file.buffer);
 
     // Upload image to Cloudinary
     const cloudinaryResult = await cloudinary.uploader.upload(localFilePath, {
-      folder: 'uploads', // Optional folder in Cloudinary
-      public_id: `image_${Date.now()}`, // Optional public ID, use a unique identifier
+      folder: 'uploads', 
+      public_id: `image_${Date.now()}`, 
     });
 
     // Delete the local file after uploading to Cloudinary
     fs.unlinkSync(localFilePath);
 
-    // Response with Cloudinary URL of the uploaded image
     res.json({
       success: true,
       message: "Image uploaded successfully",
-      imageUrl: cloudinaryResult.secure_url // URL of the uploaded image from Cloudinary
+      imageUrl: cloudinaryResult.secure_url 
     });
   } catch (error) {
     console.error("Error uploading image to Cloudinary:", error);
     res.status(500).json({ message: "Error uploading image to Cloudinary" });
   }
 });
-// Route for other endpoints
+
+
 app.use("/", route);
 
 app.listen(port, () => {
